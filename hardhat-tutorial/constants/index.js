c2aa498a96ce06eda3c322bb5030ba9852883b28
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-05-11 07:21:09 +0100

    initial commit.

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,8 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x94de820fB8CfF6BBEFDb9d32D1b9247FdbE55Cf0";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+// Crypto Devs Contract Address: 0x937483F70D6EdC3d2F043340a6bF59Eda92d12C8
