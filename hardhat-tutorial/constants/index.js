af1f47d4138f8ff0716533d4283f02dcb7e6e4ad
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-05-11 08:04:46 +0100

    ch: update metadata url.

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,7 +1,7 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x94de820fB8CfF6BBEFDb9d32D1b9247FdbE55Cf0";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-six-chi.vercel.app /api/";
+const METADATA_URL = "https://nft-collection-six-chi.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
