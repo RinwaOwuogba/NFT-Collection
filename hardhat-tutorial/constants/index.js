4e24f01b0c973fa7d46c89a819c6fc304b66a9b3
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-05-11 07:59:49 +0100

    ch: point frontend to redeployed contract. ch: add api route for retriving token metadata.

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,8 +1,8 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x94de820fB8CfF6BBEFDb9d32D1b9247FdbE55Cf0";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-six-chi.vercel.app /api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
-// Crypto Devs Contract Address: 0x937483F70D6EdC3d2F043340a6bF59Eda92d12C8
+// Crypto Devs Contract Address: 0xF8b23E2B56F1305bd0639c205117Abc0EAf5290A
